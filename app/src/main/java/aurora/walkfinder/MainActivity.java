3ed081cfc257c9ac33040b9428aa83bd02ced258
Rachel <rt.wiens@gmail.com>
2015-01-04 16:47:02 -0500

    Address now displayed by postal address instead of longitude and latitude.

--- app/src/main/java/aurora/walkfinder/MainActivity.java
+++ app/src/main/java/aurora/walkfinder/MainActivity.java
@@ -1,10 +1,16 @@
 package aurora.walkfinder;
 
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.IntentSender;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +23,10 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.ErrorDialogFragment;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
@@ -99,6 +109,10 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         if( mLastLocation != null ) {
             startLocEditTxt.setText(mLastLocation.toString());
+            // get the postal address
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD && Geocoder.isPresent() ) {
+                (new GetAddressTask(this)).execute(mLastLocation);
+            }
         }
     }
 
@@ -189,5 +203,69 @@ public class MainActivity extends ActionBarActivity implements GoogleApiClient.C
     protected void onSaveInstanceState( Bundle outState ) {
         super.onSaveInstanceState(outState);
         outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+    }
+
+    /**
+     * Get a location address. Task runs in the background to avoid delaying other tasks. 
+     */
+    private class GetAddressTask extends AsyncTask<Location, Void, String> {
+        Context mContext;
+        
+        public GetAddressTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        /**
+         * Look up address for a location based on its latitude and longitude.
+         * @param params : one or more location objects. Only returns address for the first.
+         * @return string containing address of the first location, empty string if no address found, or error message.               
+         */
+        @Override
+        protected String doInBackground(Location... params) {
+            Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+            Location loc = params[0];
+            
+            // get result addresses
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e1) {
+                Log.e("LocationSampleActivity", "IO Exception in getFromLocation()");
+                e1.printStackTrace();
+                return ("IO Exception trying to get address");
+            } catch (IllegalArgumentException e2) {
+                String errorString = "Illegal arguments " + Double.toString(loc.getLatitude()) + 
+                                     " , " + Double.toString(loc.getLongitude()) + " passed to address service.";
+                Log.e("LocationSampleActivity", errorString);
+                e2.printStackTrace();
+                return errorString;
+            }
+            
+            // if the reverse geocode returned an address
+            if( addresses != null && addresses.size() > 0) {
+                // get the first address
+                Address address = addresses.get(0);
+                // format the address (if available)
+                String addressText = String.format( "%s, %s, %s", 
+                          address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getLocality(),          // city
+                        address.getAdminArea()         // province/state
+                );
+                return addressText;
+            } else {
+                return "";
+            }
+            
+        }
+
+        /**
+         * Called after doInBackground completes.
+         * Update the text to show the postal address instead of latitude and longitude. 
+         */
+        @Override
+        protected void onPostExecute(String address) {
+            startLocEditTxt.setText(address);
+        }
     }
 }
